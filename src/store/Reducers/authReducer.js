8e9a73ae22a3f8a792cab8245773560960126430
manhbd <manhbd.work@gmail.com>
2024-11-21 00:29:57 +0700

    Update authReducer to call api admin-login

--- src/store/Reducers/authReducer.js
+++ src/store/Reducers/authReducer.js
@@ -4,12 +4,12 @@ import api from "../../api/api";
 export const admin_login = createAsyncThunk(
     'auth/admin_login',
     async(info) => {
-        console.log(info);
-        try{
-            /*const {data} = await api.post('/admin-login', info, {withCredentials: true});
-            console.log(data);*/
-        }catch (error){
-
+        try {
+            console.log(info);
+            const {data} = await api.post('/admin-login', info, {withCredentials: true});
+            console.log(data);
+        } catch (error) {
+            console.log(error.response.data);
         }
     }
 )
